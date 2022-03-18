28b059bdce2842d1d5e4e88acb989809ec8df153
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:49:43 +0300

    Добавьте файлы проекта.

---
+++ task7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int goldToCrystalCourse = 10;
+           
+
+            Console.WriteLine("Введите количество золота: ");
+
+
+            int amountOfGold = Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.WriteLine("Курс 1 кристал за "   + goldToCrystalCourse + " золота, сколько кристалов купить?");
+
+            int crystalToBuy = Convert.ToInt32(Console.ReadLine());
+            
+
+
+            amountOfGold = amountOfGold - goldToCrystalCourse * crystalToBuy;
+
+
+            Console.WriteLine("Осталось золота: " + amountOfGold + ", осталось кристалов: " + crystalToBuy);
+
+
+            
+        }
+    }
+}
