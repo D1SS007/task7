64666888e5ed7d73281376ba2090c16f2f2cf916
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-03-18 19:25:17 +0300

    Update Program.cs

--- task7/Program.cs
+++ task7/Program.cs
@@ -5,30 +5,20 @@ namespace task7
     class Program
     {
         static void Main(string[] args)
-        {
-            
-            int goldToCrystalCourse = 10;
-           
+        {            
+            int goldToCrystalCourse = 10;           
 
             Console.WriteLine("Введите количество золота: ");
 
-
-            int amountOfGold = Convert.ToInt32(Console.ReadLine());
-           
+            int amountOfGold = Convert.ToInt32(Console.ReadLine());           
 
             Console.WriteLine("Курс 1 кристал за "   + goldToCrystalCourse + " золота, сколько кристалов купить?");
 
-            int crystalToBuy = Convert.ToInt32(Console.ReadLine());
-            
-
+            int crystalToBuy = Convert.ToInt32(Console.ReadLine());          
 
             amountOfGold = amountOfGold - goldToCrystalCourse * crystalToBuy;
 
-
-            Console.WriteLine("Осталось золота: " + amountOfGold + ", осталось кристалов: " + crystalToBuy);
-
-
-            
+            Console.WriteLine("Осталось золота: " + amountOfGold + ", осталось кристалов: " + crystalToBuy);            
         }
     }
 }
